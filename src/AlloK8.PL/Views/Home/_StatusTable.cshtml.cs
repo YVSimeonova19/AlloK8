0db19ac03ec0b35c3bca6e17619d05314671e133
Yoanna Simeonova <yvsimeonova19@codingburgas.bg>
2024-12-03 17:25:58 +0200

    Add template

---
+++ src/AlloK8.PL/Views/Home/_StatusTable.cshtml.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlloK8.PL.Views.Home;
+
+public class _StatusTable : PageModel
+{
+}
---
+++ src/AlloK8.PL/Views/Shared/_Navbar.cshtml.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlloK8.PL.Views.Shared;
+
+public class _Navbar : PageModel
+{
+}
