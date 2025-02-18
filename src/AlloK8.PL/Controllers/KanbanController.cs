341086ae05551f1be8529f89454445cf70229ad1
Yoanna Simeonova <yvsimeonova19@codingburgas.bg>
2025-02-18 23:39:37 +0200

    Add basic kanban

---
+++ src/AlloK8.PL/Controllers/KanbanController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlloK8.PL.Controllers;
+
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+public class KanbanController : Controller
+{
+    [HttpGet("/kanban")]
+    public async Task<IActionResult> Kanban()
+    {
+        return this.View();
+    }
+}
