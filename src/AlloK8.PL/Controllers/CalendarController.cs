0d06940ff20a3d94298dff4658735ce40cb2fbde
Yoanna Simeonova <yvsimeonova19@codingburgas.bg>
2025-03-09 18:11:46 +0200

    Add calendar component

---
+++ src/AlloK8.PL/Controllers/CalendarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlloK8.PL.Controllers;
+
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+public class CalendarController : Controller
+{
+    [HttpGet("/calendar")]
+    public async Task<IActionResult> Calendar()
+    {
+        return this.View();
+    }
+}
